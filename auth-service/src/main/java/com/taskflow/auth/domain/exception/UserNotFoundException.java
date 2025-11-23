b474fae4e5382ba8ca3ecbbc39e100205aee1d39
Emori-Renan <emorirenan@gmail.com>
2025-11-23 18:46:14 +0900

    test3

--- auth-service/src/main/java/com/taskflow/auth/domain/exception/UserNotFoundException.java
+++ auth-service/src/main/java/com/taskflow/auth/domain/exception/UserNotFoundException.java
@@ -2,6 +2,6 @@ package com.taskflow.auth.domain.exception;
 
 public class UserNotFoundException extends DomainException {
     public UserNotFoundException(String userId) {
-        super("User with ID " + userId + " not found.");
+        super("User with ID " + userId + " not found. ");
     }
 }
