6be54f3659b0863f28c28339c04852602e28b817
Emori-Renan <emorirenan@gmail.com>
2025-11-30 22:33:58 +0900

    fixing image build gateway

--- gateway-service/src/main/java/com/taskflow/gateway/config/RedisConfig.java
+++ gateway-service/src/main/java/com/taskflow/gateway/config/RedisConfig.java
@@ -1,7 +1,9 @@
 package com.taskflow.gateway.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
@@ -11,12 +13,13 @@ import org.springframework.data.redis.serializer.RedisSerializationContext;
 public class RedisConfig {
 
     @Bean
+    @Primary
     public ReactiveRedisConnectionFactory reactiveRedisConnectionFactory() {
         return new LettuceConnectionFactory("redis", 6379);
     }
 
     @Bean
-    public ReactiveRedisTemplate<String, String> reactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
+    public ReactiveRedisTemplate<String, String> reactiveRedisTemplate(@Qualifier("reactiveRedisConnectionFactory") ReactiveRedisConnectionFactory factory) {
         return new ReactiveRedisTemplate<>(factory, RedisSerializationContext.string());
     }
 }
